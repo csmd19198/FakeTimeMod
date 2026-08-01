dea4fe09c8b9940f1824277b753e81c47338f97c
FakeTimeMod <faketime@local>
2026-08-01 15:15:15 +0800

    chore: scaffold Forge 1.20.1 mod project (MDK, gradle, mixin config)

---
+++ src/main/java/com/faketime/FakeTimeMod.java
@@ -0,0 +1,11 @@
+package com.faketime;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FakeTimeMod.MODID)
+public class FakeTimeMod {
+    public static final String MODID = "faketimemod";
+
+    public FakeTimeMod() {
+    }
+}
---
+++ src/main/java/com/faketime/mixin/LevelMixin.java
@@ -0,0 +1,8 @@
+package com.faketime.mixin;
+
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Level.class)
+public class LevelMixin {
+}
